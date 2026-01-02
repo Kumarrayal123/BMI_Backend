2abedf8448f089525b48a7d38f7343725f285bdc
Kumarrayal123 <kardamkumar13@gmail.com>
2026-01-02 19:20:40 +0530

    kk

--- models/camp.js
+++ models/camp.js
@@ -1,20 +1,24 @@
-  import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-  const campSchema = new mongoose.Schema(
-    {
-      name: {
-        type: String,
-        required: true   // Camp-1, Camp-2
-      },
-      location: {
-        type: String,
-        required: true   // Tolichowki
-      },
-      address: String,
-      date: String,      // 12-12-2025
-      time: String       // 10:00 AM - 4:00 PM
+const campSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true   // Camp-1, Camp-2
     },
-    { timestamps: true }
-  );
+    location: {
+      type: String,
+      required: true   // Tolichowki
+    },
+    address: String,
+    date: String,      // 12-12-2025
+    time: String,      // 10:00 AM - 4:00 PM
+    volunteers: {
+      type: [String],
+      default: []
+    }
+  },
+  { timestamps: true }
+);
 
-  export default mongoose.model("Camp", campSchema);
+export default mongoose.model("Camp", campSchema);
